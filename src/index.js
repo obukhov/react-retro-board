835f1d38895271bf0966ae0d5bb5c89954716b9a
obukhov <a.a.obukhov@gmail.com>
2019-06-17 00:33:14 +0200

    Add click handler

--- src/index.js
+++ src/index.js
@@ -1,16 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import uuidv4 from 'uuid/v4'
 import './index.css';
 
+
 class Retro extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            columns: [
+                {
+                    id: 0,
+                    header: 'Start',
+                    cards: [],
+                }, {
+                    id: 1,
+                    header: 'Stop',
+                    cards: [],
+                }, {
+                    id: 2,
+                    header: 'Continue',
+                    cards: [],
+                },
+            ]
+        }
+    }
+
     render() {
+        const columns = this.state.columns.map((item, i) =>
+            <Column
+                key={item.id}
+                header={item.header}
+                cards={item.cards}
+                addCardCallback={(text) => this.addCardToColumn(i, text)}
+            />
+        )
+
         return (
             <div className="retro-board">
-                <Column header="Start" />
-                <Column header="Stop" />
-                <Column header="Continue" />
+                {columns}
             </div>
         )
+    }
+
+    addCardToColumn(i, text) {
+        var columns = this.state.columns.slice()
+        columns[i].cards.push({ id: uuidv4(), text: text })
+
+        this.setState({ columns: columns })
     }
 }
 
@@ -19,8 +56,8 @@ class Column extends React.Component {
         return (
             <div className="retro-column">
                 <h1 className="header">{this.props.header}</h1>
-                <Card text="foo" />
-                <Card text="bar" />
+                {this.props.cards.map((item) => <Card key={item.id} text={item.text} />)}
+                <button onClick={(e) => this.props.addCardCallback('bla')}>Add</button>
             </div>
         )
     }
