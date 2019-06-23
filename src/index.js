06d14cdc919d86143de51f0246fc20b7cd989db4
obukhov <a.a.obukhov@gmail.com>
2019-06-23 18:15:39 +0200

    Separate css files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import './index.css';
 import Retro from './components/Retro'
+import './index.css';
 
 ReactDOM.render(
     <Retro />,
