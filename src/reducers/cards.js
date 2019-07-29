f3658998ee5b01ce8398e54066e5840767ac5bed
obukhov <a.a.obukhov@gmail.com>
2019-07-29 23:21:31 +0200

    Use deconstruction and fix bug (filter instead of map)

--- src/reducers/cards.js
+++ src/reducers/cards.js
@@ -4,25 +4,23 @@ import uuidv4 from 'uuid/v4'
 const cards = (state = [], action) => {
     switch (action.type) {
         case actions.ADD_CARD:
-            return state.concat([{
-                id: uuidv4(),
-                column: action.column,
-                text: action.text,
-                editing: true
-            }]) 
+            return [
+                ...state,
+                {
+                    id: uuidv4(),
+                    column: action.column,
+                    text: action.text,
+                    editing: true
+                }
+            ]
         case actions.DELETE_CARD:
-            return state.filter(card => (card.id !== action.id)) 
+            return state.filter(card => (card.id !== action.id))
 
-        case actions.UPDATE_CARD_TEXT: 
-            return state.filter(card => { 
-                if (card.id === action.id) {
-                     return Object.assign(card, {text: action.text});  
-                }; 
-                return card; 
-            })
+        case actions.UPDATE_CARD_TEXT:
+            return state.map(card => card.id === action.id ? { ...card, text: action.text } : card);
 
-        case actions.SET_CARD_EDIT_MODE: 
-            return state.filter(card => { if (card.id === action.id) { card.editing = action.mode }; return card; })
+        case actions.SET_CARD_EDIT_MODE:
+            return state.map(card => card.id === action.id ? { ...card, editing: action.mode } : card)
 
         default:
             return state
